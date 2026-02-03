9a32833ab5dd0ab38d5f4c8876f496d23879f3a7
Buvaneswaran E <168317978+BhuvaneshGg@users.noreply.github.com>
2026-02-03 22:44:18 +0530

    Fixed the Bug On Reward collection

--- Pix.py
+++ Pix.py
@@ -1,4 +1,10 @@
 from seleniumbase import SB
+from Accounts import getEmailPair,getLength
+from pathlib import Path
+
+img_dir = Path("logs/images")
+img_dir.mkdir(parents=True, exist_ok=True)
+
 
 def login(email:str , password:str , i):
     with SB(
@@ -13,39 +19,48 @@ def login(email:str , password:str , i):
             sb.click("span:contains('Continue with Email')")
             sb.type("#email-input",email)
             sb.type("#password-input",password)
-            sb.sleep(4)
             sb.wait_for_element_clickable(
                 "span:contains('Login')",
                 timeout=20
             )
+            sb.sleep(0.5)
             sb.click("span:contains('Login')")
-            sb.sleep(2)
             reward_selector = "span:contains('Claim 10,000 daily credits')"
             close_button = "span:contains('Close')"
+            toast_close_btn = "button[aria-label='close']"
+            sb.sleep(2)
+
             if sb.is_element_visible(reward_selector):
-                sb.sleep(3)
-                print("✅ Reward available")
-                sb.save_screenshot(f"reward{i}.png")
+                sb.sleep(2)
+                print(f"✅ Reward available on Email: {email}")
+                #sb.save_screenshot(f"reward{i}.png")
                 sb.wait_for_element_clickable(reward_selector, timeout=30)
                 sb.click(reward_selector)
+                if (sb.is_element_visible(reward_selector)):
+                    sb.sleep(1)
+                    if (sb.is_element_visible(reward_selector)):
+                        sb.sleep(4)
+                        sb.click(reward_selector)
+
                 sb.click(close_button)
-                sb.sleep(3)
+                sb.wait_for_element_visible(toast_close_btn, timeout=15)
+                sb.click(toast_close_btn)
             else:
-                print("❌ Reward already claimed today")
-            sb.wait(3)
+                print(f"❌ Reward already claimed today on Email: {email}")
             sb.click("header button[aria-haspopup='true']")
             print("clicked tthe icon")
             sb.click("div:contains('Log out')")
-            sb.save_screenshot(f"log{i}.png")
-        except :
-            print("Error Founded")
+            
+        except:
+            print(f"Error Founded on Email: {email}")
+            sb.save_screenshot(str(img_dir / f"Error{i}.png"))
+
+def main():
+    for i in range(0,getLength()):
+        data = getEmailPair(i)
+        login(data["email"],data["password"],i)
+    print("sucessfully Completed")
 
 
 
-
-
-data = [ #add a dictonary of Email and Password
-]
-for i in range(len(data)):
-    login(data[i]["email"],data[i]["password"],i)
-print("sucessfully Completed")
+main()
